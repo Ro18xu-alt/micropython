28741ff30b87530adec220cc30e7d0393c80b5ca
Ro18xu-alt <vincenzo.zingone97@gmail.com>
2026-07-09 17:01:45 +0200

    Add files via upload

--- PC-UART.py
+++ PC-UART.py
@@ -3,13 +3,28 @@ from contextlib import contextmanager
 import serial
 import time
 import json
+from colorama import Fore
 
 baudrate = 9600
 port = "COM28"
 timeout = 20
 
-connessioni_attese = (('A0', 'D51'), ('D21', 'D12'), ('D16', 'D15'), ('D24', 'D10'), ('D9', 'D25'))
-
+iso_spi_fault_u212_pin = (('A0', 'D51'), ('D51', 'A0'), ("D53", 'A2'), ('A2', 'D53'), ("D54", "A3"), ('A3', 'D54'), ("A1", "D52"), ("A1", "D52"))
+iso_spi_fault_u212_dizionario = {
+    "A0" : "SX J1INA_1 ISOB N",
+    "D51": "DX J1OUTA_1 ISOA N",
+    
+    "A1":  "SX J1INA_2 ISOB P",
+    "D52": "DX J1OUTA_2 ISOA P",
+    
+    "A2":  "SX J1INB_3 FAULT IN",
+    "D53": "DX J1OUTA_3 FAULT OUT",
+    
+    "A3" : "SX J1INB_4 FAULT RTN IN",
+    "D54": "DX J1OUTB_4 FAULT RTN OUT"
+    
+    
+}
 
 class SerialComm:
     
@@ -26,7 +41,7 @@ class SerialComm:
     def __exit__(self, exc_type, exc, tb):
         if self._ser:
             self._ser.close()
-        print("Chiusura dell'interfaccia seriale")
+        print("\n\nChiusura dell'interfaccia seriale")
 
     def read_line(self, timeout=20):
         deadline = time.time() + timeout
@@ -65,12 +80,30 @@ def valida_harness(connessioni_rilevate, connessioni_attese):
     inattese = rilevate_set - attese_set
     
     return mancanti, inattese
+
+def stampa_report(mancanti, inattese, dizionario: dict):
+
+    if not mancanti and not inattese:
+        print(f"\n\n{Fore.GREEN}OK -  HARNESS VALIDO{Fore.RESET}, tutte le connessioni corrispondono")
+        return     
+    else:
+        print(f"\n\n{Fore.RED}KO - HARNESS NON VALIDO{Fore.RESET}, controllare le connessioni mancanti o errate")
+        if mancanti:
+            print(f"\nConnessioni {Fore.RED}MANCANTI{Fore.RESET}: {len(mancanti)}")
+            for c in sorted(mancanti, key=sorted):
+                a, b = c
+                print(f"{(dizionario.get(a, a)).center(5)}  <->  {dizionario.get(b, b).center(5)}")
+        if inattese :
+            print(f"\nConnessioni {Fore.YELLOW}INATTESE{Fore.RESET}: {len(inattese)}")
+            for c in sorted(inattese, key=sorted):
+                a, b = c
+                print(f"! {dizionario.get(a, a).center(5)}  <->  {dizionario.get(b, b).center(5)}") 
         
         
 def main():
     print("PC-UART")
     
-    with SerialComm(port="/dev/ttyACM1") as s:
+    with SerialComm() as s:
         
         s.handshake()
         s._ser.write(b"HARNESS TEST\n") #type: ignore
@@ -82,17 +115,14 @@ def main():
         #list(line)
         # print(f"stampo line: {line}")
         # print(f"stampo type line : {type(line)}")
-        #print(line_raw)
         try:
             connessioni_rilevate = [tuple(coppia) for coppia in json.loads(line_raw)]
-            #print(connessioni_rilevate)
         except Exception:
             raise ValueError("Errore nella conversione del file json")
         
-        mancanti, inattese = valida_harness(connessioni_rilevate=connessioni_rilevate, connessioni_attese=connessioni_attese)
-
-        print(f"connessioni mancanti: {mancanti}")
-        print(f"connessioni inattese: {inattese}")
+        mancanti, inattese = valida_harness(connessioni_rilevate=connessioni_rilevate, connessioni_attese=iso_spi_fault_u212_pin, )
+        stampa_report(mancanti, inattese, iso_spi_fault_u212_dizionario)
+                
     pass
 
 main()
